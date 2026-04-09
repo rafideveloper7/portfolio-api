c7ef419a10fb14e432724c83c3babf6009c16364
rafideveloper7 <rafideveloper7@gmail.com>
2026-04-09 19:51:42 +0500

    make serverless func

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import { app } from './app.js'
 import { env } from './config/env.js'
 
-app.listen(env.port, () => {
-  console.log(`Backend running on http://localhost:${env.port}`)
-})
+// app.listen(env.port, () => {
+//   console.log(`Backend running on http://localhost:${env.port}`)
+// })
+
+// serverless deployment entry point (e.g. for Vercel)
+export default app;
